fb40611c5608ba7d6c18d9adac30f221cb61dbd1
Rococo Scarlet <897479234@qq.com>
2022-07-06 11:35:01 +0800

    修改人鱼文本，修复蘑菇融合的bug

--- expansions/script/c110000005.lua
+++ expansions/script/c110000005.lua
@@ -117,7 +117,7 @@ function c110000005.fusop(e,tp,eg,ep,ev,re,r,rp)
 		local fgroup=ce:GetTarget()
 		mg3=fgroup(ce,e,tp)
 		local mf=ce:GetValue()
-		sg2=Duel.GetMatchingGroup(c110000005.fusfilter2,,tp,LOCATION_EXTRA,0,nil,e,tp,mg3,chkf,mf)
+		sg2=Duel.GetMatchingGroup(c110000005.fusfilter2,tp,LOCATION_EXTRA,0,nil,e,tp,mg3,chkf,mf)
 	end
 	if sg1:GetCount()>0 or (sg2~=nil and sg2:GetCount()>0) then
 		local sg=sg1:Clone()
